1609b9f9d89c1b645e0830e8d9c1dc7080a80536
RoiHirsh <sharpstudio.xyz@gmail.com>
2023-03-02 23:36:00 +0200

    logic completed

--- static/homepage.js
+++ static/homepage.js
@@ -3,9 +3,11 @@ function template(data) {
     tpl.querySelector(".cell-a").textContent = data[0];
     tpl.querySelector(".cell-b").textContent = data[1];
     tpl.querySelector(".cell-c").innerHTML = `<a href="http://${data[3]}" target="_blank">${data[2]}</a>`;
-    tpl.id = data[4];
+    tpl.querySelector(".value-wrapper").setAttribute("id", data[4]);
     return tpl;
-  }
+}
+
+let templateDict = {};
   
 function toggleButton(button) {
     button.classList.toggle("active");
@@ -20,8 +22,16 @@ function toggleButton(button) {
                     // Create the HTML template for the values
                     let container = document.getElementById("values-container");
                     for (let i = 0; i < data.length; i++) {
-                        container.insertBefore(template(data[i]), container.firstChild);
+                        let templateId = data[i][4];
+                        if (!templateDict.hasOwnProperty(templateId)) {
+                            impact = template(data[i]);
+                            container.insertBefore(impact, container.firstChild);
+                            templateDict[templateId] = [button.id];
+                        } else if (!templateDict[templateId].includes(button.id)) {
+                            templateDict[templateId].push(button.id);
+                        }
                     }
+                    console.log(templateDict)
                 } else {
                     console.log("No values for column " + button.id);
                 }
@@ -29,8 +39,22 @@ function toggleButton(button) {
             .catch(error => {
                 console.error(error);
             });
-    } else {
-        console.log("Button " + button.id + " was toggled off");
+        } else {
+            console.log("Button " + button.id + " was toggled off");
+            for (let templateId in templateDict) {
+                let buttonIds = templateDict[templateId];
+                let index = buttonIds.indexOf(button.id);
+                if (index !== -1) {
+                  buttonIds.splice(index, 1);
+                  if (buttonIds.length === 0) {
+                    let template = document.getElementById(`${templateId}`);
+                    console.log(template)
+                    template.parentNode.removeChild(template);
+                    delete templateDict[templateId];
+                  }
+                }
+              }
+            console.log(templateDict)
     }
 }
 
